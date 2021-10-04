17115efc003f6b763f1ecfaa9a387797efcb8f69
Roices <tuande83@gmail.com>
2021-10-04 09:34:44 +0700

    Commit code

---
+++ MyWalletApp/MainsView/WalletView/DetailSpendingInWallet/DetailAccumulation.swift
@@ -0,0 +1,29 @@
+//
+//  DetailAccumulation.swift
+//  MyWalletApp
+//
+//  Created by Tuan on 26/09/2021.
+//
+
+import UIKit
+
+class DetailAccumulation: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
